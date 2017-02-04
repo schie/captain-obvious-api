18267a6fa086756cf7ac54999aa98ddc898cb3f5
Dustin Schie <me@dustinschie.com>
2017-02-04 18:53:42 -0500

    Slightly more efficient mapping
    * removes slicing from `help` endpoint
    * removes gif extension presumption
      * files no longer _have to be_ GIFs
    * :pizza:

--- app.rb
+++ app.rb
@@ -1,11 +1,14 @@
 require 'bundler'
 Bundler.require
 
+file_names = Dir.entries('public')[2..-1]
+base_names = file_names.map { |name| File.basename(name, File.extname(name)) }
 
 get '/', '/help' do
-  json Dir.entries('public')[2..-1].map { |name| { route: "/#{name[0..-5]}", filename: name, action: name[0..-5].tr('_', ' ') } }
+  json base_names.map { |name| { route: "#{request.base_url}/#{name}", path: "/#{name}", action: name.tr('_', ' ') } }
 end
 
 get '/:action' do
-  send_file File.join(settings.public_folder, "#{params[:action]}.gif")
+  file_name = file_names.select { |file| file =~ /#{params[:action]}/}
+  send_file File.join(settings.public_folder, file_name)
 end
